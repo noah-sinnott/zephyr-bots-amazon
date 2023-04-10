4e35d93a974f13c6236d88230b1c056389f4ff60
Noah <noah.sinnott123@gmail.com>
2023-04-10 13:43:16 +0100

    reset with nw

---
+++ echo.py
@@ -0,0 +1,6 @@
+import sys
+
+for v in sys.argv[1:]:
+    print(v)
+
+# Open the Python output page and the GitHub repository
--- src/renderer/scripts/amazonCheckout.js
+++
@@ -1,7 +0,0 @@
-// import puppeteer from 'puppeteer';
-
-export default async function amazonCheckout(email, password, url, maxPrice, refreshRate, endpoint, amount) {
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage()
-  // await page.goto('https://developer.chrome.com/');
-}
